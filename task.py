67f001b48bbf3886a8e2f70b12d19c32850080b7
İREM NUR YILDIRIM <irem.yildirim1@boun.edu.tr>
2023-12-14 12:36:38 +0300

    task.py new version,row column customized,1min exc

--- task.py
+++ task.py
@@ -2,8 +2,8 @@ import cache_module
 import numpy
 
 # Prepare an RGB image containing 3 colour channels.
-ROW = 1024
-COL = 2048
+ROW = 32
+COL = 64
 Channel = 3
 image = numpy.random.randint(0, 256, size=(ROW, COL, Channel), 
 dtype=numpy.int64)
@@ -15,8 +15,7 @@ down = -10
 mask = numpy.random.randint(down, up + 1, size=(mask_size, mask_size), 
 dtype=numpy.int64)
 
-# Prepare an empty result image. You will fill this empty array with your 
-code.
+# Prepare an empty result image. You will fill this empty array with your code. 
 result = numpy.zeros([ROW, COL, Channel], dtype=numpy.int64)
 
 # Configuration for the cache simulator module.
@@ -29,14 +28,48 @@ cm = cache_module.cache_module(l1, l2, l3, m)
 ###### WRITE YOUR CODE BELOW. ######
 
 # 1. Load the image into the memory
+index = 0
+for i in range(ROW):
+    for j in range(COL):
+        for k in range(Channel):
+            value = image[i, j, k] & 0xFF  # Assuming 64-bit signed integers
+            cm.write(index, value)
+            index += 1
 
 
 # 2. Traverse the image array and apply the mask. Write the results into 
-the memory through the write function. Do not fill the result array in 
-this step.
+ # the memory through the write function. Do not fill the result array in 
+ # this step.
+index = 0
+for i in range(1, ROW - 1):
+    for j in range(1, COL - 1):
+        for k in range(Channel):
+           # Initialize the result value
+            result_value = 0
 
+            # Apply the mask considering values outside the image as zero
+            for m in range(mask_size):
+                for n in range(mask_size):
+                    # Check if indices are within bounds
+                    if 0 <= i - 1 + m < ROW and 0 <= j - 1 + n < COL:
+                        result_value += image[i - 1 + m][j - 1 + n][k] * mask[m][n]
 
+            # Convert the result value to 64-bit signed integer
+            #result_value = result_value & 0xFF
+            
+            # Ensure that memory stays 8-bit unsigned
+            cm.write(index, result_value)  # Retain only the least significant 8 bits
+            index += 1
 # 3. Load the result image from memory through the read function.
+            #and do not forget to convert 64 bit signed while reading to result array 
+            index = 0
+            for i in range(ROW):
+                for j in range(COL):
+                    for k in range(Channel):
+                        # Read the result from memory and convert to 64bit signed integer
+                        result[i, j, k] = cm.read(index) & 0xFFFFFFFFFFFFFFFF  
+                        index += 1
+            
 
 
 ###### WRITE YOUR CODE ABOVE. ######
